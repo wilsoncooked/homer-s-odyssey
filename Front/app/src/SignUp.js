69aadcfaf1ff09fd7b6774b28200567b329d2bff
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-06-03 19:56:14 +0200

    Wilsoncook Sarah 020

--- Front/app/src/SignUp.js
+++ Front/app/src/SignUp.js
@@ -4,22 +4,36 @@ class SignUp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      email: 'test@test.com',
+      firstName: '',
+      lastName: 'Bond',
+      email: 'mon@email.com',
+      password: 'monPassw0rd',
+      passwordRep: 'monPassw0rd',
     };
-    this.updateEmailField = this.updateEmailField.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  updateEmailField(e) {
+  handleChange(e) {
     this.setState({
-      email: e.target.value,
+      [e.target.name]: e.target.value,
     });
+  }
+
+  handleSubmit(e) {
+    console.log(this.state);
   }
 
   render() {
     return (
-      <div>
-        <h1>{this.state.email}</h1>
-        <input type='email' name='email' onChange={this.updateEmailField} />
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h1>{JSON.stringify(this.state, 1, 1)}</h1>
+        <input type='text' name='firstName' />
+        <input type='text' name='lastName' />
+        <input type='email' name='email' onChange={this.handleChange} />
+        <input type='password' name='password' />
+        <input type='password' name='passwordRep' />
+        <input type='submit' value='Submit' />
+      </form>
     );
   }
 }
