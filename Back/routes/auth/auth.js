518c33dfc79b40e1dc5acaaf436653eea60d5077
Sarah Wilsoncook <sarahlouise125@gmail.com>
2019-06-04 16:37:35 +0200

    040 working post

--- Back/routes/auth/auth.js
+++ Back/routes/auth/auth.js
@@ -2,11 +2,11 @@ const express = require('express');
 const connection = require('../../helpers/db');
 const router = express.Router();
 
-router.post('/signup', (req, next) => {
+router.post('/signup', (req, res, next) => {
   let post = req.body;
 
-  connection.query('INSERT INTO users SET ?', post, function(error, res) {
-    if (error) res.status(500).end();
+  connection.query('INSERT INTO users SET ?', post, function(error, result) {
+    if (error) res.sendStatus(500).end();
     res.end();
   });
 });
